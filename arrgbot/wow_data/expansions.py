4607fcf6bab859df8f2c12e732cb452b4cb05721
arrg <arrg@lorrgs.io>
2022-11-27 15:57:15 +0000

    fix Drafli Release

--- arrgbot/wow_data/expansions.py
+++ arrgbot/wow_data/expansions.py
@@ -1,4 +1,3 @@
-
 import arrow
 
 
@@ -17,21 +16,20 @@ BFA = Expansion(
     "Battle for Azeroth",
     release="2018-08-14",
     abbreviations=["bfa"],
-    img="https://wow.zamimg.com/uploads/blog/images/13411-world-of-warcraft-battle-for-azeroth-release-date-is-august-14th.jpg"
+    img="https://wow.zamimg.com/uploads/blog/images/13411-world-of-warcraft-battle-for-azeroth-release-date-is-august-14th.jpg",
 )
 
 SHADOWLANDS = Expansion(
     "Shadowlands",
     release="2020-11-23",
     abbreviations=["sl"],
-    img="https://scontent-lhr8-2.xx.fbcdn.net/v/t1.6435-9/82158322_10157061179678719_2296017863502200832_n.png?_nc_cat=102&ccb=1-5&_nc_sid=dd9801&efg=eyJpIjoidCJ9&_nc_ohc=iNSUkKzpqU8AX_HlsZ8&_nc_ht=scontent-lhr8-2.xx&oh=ea123b280e8f22c8e16083855d46dc99&oe=615089A3"
+    img="https://scontent-lhr8-2.xx.fbcdn.net/v/t1.6435-9/82158322_10157061179678719_2296017863502200832_n.png?_nc_cat=102&ccb=1-5&_nc_sid=dd9801&efg=eyJpIjoidCJ9&_nc_ohc=iNSUkKzpqU8AX_HlsZ8&_nc_ht=scontent-lhr8-2.xx&oh=ea123b280e8f22c8e16083855d46dc99&oe=615089A3",
 )
 
 
 DRAGONFLIGHT = Expansion(
     "Dragonflight",
-    release="2022-11-28",
+    release="2022-11-29",
     abbreviations=["df"],
-    img="https://i.imgur.com/U97A6MB.jpeg"
+    img="https://i.imgur.com/U97A6MB.jpeg",
 )
-
