4084166b4a0a24916a27761379f05fb196cbdfe0
arrg <arrg@lorrgs.io>
2022-12-26 21:08:12 +0000

    Update Cutoff Season

--- arrgbot/cogs/wow/wow_rio.py
+++ arrgbot/cogs/wow/wow_rio.py
@@ -7,6 +7,7 @@ from aiohttp import ClientSession
 #
 RAIDER_IO_API = "https://raider.io/api/v1"
 
+
 async def fetch_data(url, **params):
     async with ClientSession() as session:
         async with session.get(url=url, params=params) as resp:
@@ -17,7 +18,8 @@ async def fetch_data(url, **params):
 # Commands
 #
 
-async def get_cutoff(season="season-sl-4", region="eu", faction="all"):
+
+async def get_cutoff(season="season-df-1", region="eu", faction="all"):
     """Returns the Score Cutoff for the 0.1% Title."""
     url = "https://raider.io/api/v1/mythic-plus/season-cutoffs"
     data = await fetch_data(url, region=region, season=season)
